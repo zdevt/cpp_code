0d59a973163d18851d9b1c0508c5565525faf033
zdevt <zhangtao.hawk@qq.com>
2018-06-01 13:19:53 +0800

    add main.c test.c : gcc -o main main.c test.c ;without inclue

---
+++ main.c
@@ -0,0 +1,43 @@
+
+/*
+ * =========================================================================
+ *
+ *       FileName:  main.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2018-06-01 13:16:43
+ *  Last Modified:  2018-06-01 13:17:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  zt ()
+ *   Organization:
+ *
+ * =========================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+
+int main(int argc, char* argv[])
+{
+  (void)argc;
+  (void)argv;
+
+  test();
+
+  return 0;
+}
+
---
+++ test.c
@@ -0,0 +1,27 @@
+
+/*
+ * =========================================================================
+ *
+ *       FileName:  test.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2018-06-01 13:16:45
+ *  Last Modified:  2018-06-01 13:17:04
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  zt ()
+ *   Organization:
+ *
+ * =========================================================================
+ */
+
+#include <stdio.h>
+
+void test()
+{
+  printf ( "test\n" );
+}
+
